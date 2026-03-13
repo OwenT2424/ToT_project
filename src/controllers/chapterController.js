d79316c4ca3998ea543c9d81cef737ca8140a4b7
Tejas Parmar <parmart1@roehampton.ac.uk>
2026-03-13 16:26:13 +0000

    Scaffolded the controllers

---
+++ src/controllers/chapterController.js
@@ -0,0 +1,19 @@
+// SERVER BLOCK -----
+const Chapter = require("../models/chapterModel");
+const Story = require("../models/storyModel");
+
+exports.createChapter = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
+
+exports.getChapters = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
+
+exports.getChapter = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
+
+exports.updateChapter = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
---
+++ src/controllers/storyController.js
@@ -0,0 +1,14 @@
+// SERVER BLOCK -----
+const Story = require("../models/storyModel");
+
+exports.createStory = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
+
+exports.getAllStories = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
+
+exports.getStory = async (req, res) => {
+  // OWEN'S IMPLEMENTATION
+};
