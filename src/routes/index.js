a6bbff892b457cd6cf688507e44968d11797b970
Tejas Parmar <parmart1@roehampton.ac.uk>
2026-03-13 15:36:11 +0000

    Scaffold Updated - index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
+// Server Block -----
+const express = require("express");
 const router = express.Router();
-const { healthCheck } = require('../controllers/healthController');
+const { healthCheck } = require("../controllers/healthController");
+router.get("/health", healthCheck);
 
-router.get('/health', healthCheck);
+// API Routing Block -> ZAYNAH -----
 
+// auth routes can go below
+
+// stories route can go below
+
+// chapters route can go below
+
+// Server Block -----
 module.exports = router;
