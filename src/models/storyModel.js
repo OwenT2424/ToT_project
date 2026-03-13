bcbf1846fbb3b7e3d576ddeb209039002afbec02
Tejas Parmar <parmart1@roehampton.ac.uk>
2026-03-13 16:13:51 +0000

    Scaffolded the models

---
+++ src/models/storyModel.js
@@ -0,0 +1,10 @@
+// Server Block ------
+const pool = require('../config/db');
+const { v4: uuidv4 } = require('uuid');
+
+const Story = {
+  // Create functions here -> KRIS
+};
+
+// Server Block ------
+module.exports = Story;
---
+++ src/models/userModel.js
@@ -0,0 +1,10 @@
+// Server Block -----
+const pool = require("../config/db");
+const { v4: uuidv4 } = require("uuid");
+
+const User = {
+  // Create functions here - KRIS
+};
+
+// Server Block -----
+module.exports = User;
