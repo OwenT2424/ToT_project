289a5b52eb8c0b2e5e249761400e83bb074931e6
Tejas Parmar <parmart1@roehampton.ac.uk>
2026-03-14 00:32:38 +0000

    Added file for authController.js

--- src/controllers/authController.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcrypt");
-const User = require("../models/userModel");
-const SALT_ROUNDS = 10;
-
-// OWEN DOES NOT NEED TO IMPLEMENT THIS!!!
-// TEJAS IS RESPONSIBLE TO SETUP THIS CONTROLLER
